60f032a57177a8d0d4addecbe02c1ade246389ee
hasnaa19 <66091289+hasnaa19@users.noreply.github.com>
2022-05-23 19:01:12 +0200

    Add files via upload

---
+++ ImageFilters/AdaptiveFilter.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageFilters
+{
+    class AdaptiveFilter
+    {
+        public static byte[,] filter(byte[,] ImageMatrix, int Ws, string c)
+        {
+            byte[] a; //O(1)
+            byte Zmed;  //O(1)
+            byte Zmin;  //O(1)
+            byte Zmax;  //O(1)
+            byte A1;  //O(1)
+            byte A2;  //O(1)
+            byte B1;  //O(1)
+            byte B2;  //O(1)
+            int windowsize;  //O(1)
+            for (int i = 0; i < ImageMatrix.GetLength(0); i++)//O(row)
+            {
+                for (int j = 0; j < ImageMatrix.GetLength(1); j++)//O(columns)
+                {
+                    windowsize = 3;//O(1)
+
+                    while (true)
+                    {
+                        var arlist1 = new ArrayList();//O(1)
+                        for (int ii = i - (windowsize / 2); ii <= i + (windowsize / 2); ii++)//O(windowsize)
+                        {
+
+                            if (ii < 0 || ii >= ImageMatrix.GetLength(0))//O(1)
+                            {
+
+                                continue;//O(1)
+                            }
+
+                            for (int jj = j - (windowsize / 2); jj <= j + (windowsize / 2); jj++)//O(windowsize)
+                            {
+                                if (jj < 0 || jj >= ImageMatrix.GetLength(1))//O(1)
+                                {
+
+                                    continue;//O(1)
+                                }
+
+                                arlist1.Add(ImageMatrix[ii, jj]);//O(1)
+
+                            }
+
+                        }
+                        a = new byte[arlist1.Count];//O(1)
+                        for (int k = 0; k < arlist1.Count; k++)//O(arlist1.Count)
+                        {
+                            a[k] = (Byte)arlist1[k];//O(1)
+                        }
+
+                        if(c == "Counting Sort")//O(1)
+                        {
+                            a = Sorting.CountingSort(a, a.Length);//O(n + k)
+                        }
+                        else if (c == "Quick Sort")//O(1)
+                        {
+                            a = Sorting.quicksort(a, 0, a.Length - 1);//O(N^2)
+                        }
+
+                        if(a.Length % 2 == 0 )//O(1)
+                        {
+                            int x = (a[(a.Length / 2) - 1] + a[(a.Length / 2)]) / 2;  //O(1)
+                            Zmed = (byte)x;    //O(1)
+                        }
+                        else
+                        {
+                            Zmed = a[(a.Length / 2)];  //O(1)
+                        }
+
+                        Zmin = a[0];    //O(1)
+                        Zmax = a[a.Length - 1];   //O(1)
+                        A1 = (byte)(Zmed - Zmin);   //O(1)
+                        A2 = (byte)(Zmax - Zmed);   //O(1)
+
+                        if (A1 <= 0 || A2 <= 0)  //O(1)
+                        {
+                            windowsize += 2;   //O(1)
+                            if (windowsize > Ws)   //O(1)
+                            {
+                                ImageMatrix[i, j] = Zmed;  //O(1)
+                                break;  
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                        else
+                        {
+                            B1 = (byte)(ImageMatrix[i, j] - Zmin);  //O(1)
+                            B2 = (byte)(Zmax - ImageMatrix[i, j]);   //O(1)
+                            if (B1 <= 0 || B2 <= 0)  //O(1)
+                            {
+                                ImageMatrix[i, j] = Zmed;  //O(1)
+                            }
+                            break;  
+                        }
+
+                    }
+
+                }
+
+            }
+            return ImageMatrix;  //O(1)
+        }
+    }
+}
---
+++ ImageFilters/AlphaTrim.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageFilters
+{
+    class AlphaTrim
+    {
+        public static byte[,] alphaTrim_Counting(byte[,] ImageMatrix, int windowSize, int trimValue)//O(n*m^3) or O(n^3 *m)
+        {
+
+            byte[,] result = new byte[ImageMatrix.GetLength(0), ImageMatrix.GetLength(1)];//O(1)
+            int limit = windowSize / 2;//O(1)
+            for (int i = 0; i < ImageMatrix.GetLength(0); i++)     //O(rows)
+            {
+                for (int j = 0; j < ImageMatrix.GetLength(1); j++)     //O(columns)
+                {
+                    //__________GETTING WINDOW____________
+                    byte[] window = new byte[windowSize * windowSize];     //O(1)
+                    int window_index = 0; //O(1)
+
+                    for (int ii = i - limit; ii <= i + limit; ii++)   // O(windowSize)
+                    {
+
+                        if (ii < 0 || ii >= ImageMatrix.GetLength(0)) //O(1)
+                        {
+
+                            continue; //O(1)
+                        }
+
+                        for (int jj = j - limit; jj <= j + limit; jj++)  //O(windowSize)
+                        {
+                            if (jj < 0 || jj >= ImageMatrix.GetLength(1)) //O(1)
+                            {
+
+                                continue; //O(1)
+                            }
+
+                            window[window_index] = ImageMatrix[ii, jj]; //O(1)
+                            window_index++; //O(1)
+                        }
+                    }
+
+                    //________SORTING WINDOW USING COUNT SORT_________
+
+                    Sorting.CountingSort(window, window.Length);   //O(n+k)
+
+                    //___________TRIM & CALC AVERAGE__________
+                    int sum = 0; //O(1)
+                    for (int x = trimValue; x < window.Length - trimValue; x++)//O(windowSize)^2    omega(1)
+                    {
+                        sum += window[x];   //O(1)
+                    }
+                    byte avg = (byte)(sum / (window.Length - (trimValue * 2)));//O(1)
+                    //_________UPDATE RESULT MATRIX___________
+                    result[i, j] = avg;//O(1)
+                }
+            }
+            return result;//O(1)
+        }
+
+
+        public static byte[,] alphaTrim_KTH(byte[,] ImageMatrix, int windowSize, int trimValue)
+        {
+
+            byte[,] result = new byte[ImageMatrix.GetLength(0), ImageMatrix.GetLength(1)];   //O(1)
+            int k = trimValue;     //O(1)
+            for (int i = 0; i < ImageMatrix.GetLength(0); i++)           //O(rows)
+            {
+                for (int j = 0; j < ImageMatrix.GetLength(1); j++)           //O(columns)
+                {
+
+                    int Sum = 0;  //O(1)
+                    int counter = 0;    //O(1)
+                    byte[] window = new byte[windowSize * windowSize];   //O(1)
+                    int window_index = 0;  //O(1)
+
+                    for (int ii = i - (windowSize / 2); ii <= i + (windowSize / 2); ii++)  // O(windowSize)
+                    {
+
+                        if (ii < 0 || ii >= ImageMatrix.GetLength(0))   //O(1)
+                        {
+
+                            continue;  //O(1)
+                        }
+
+                        for (int jj = j - (windowSize / 2); jj <= j + (windowSize / 2); jj++) // O(windowSize)
+                        {
+                            if (jj < 0 || jj >= ImageMatrix.GetLength(1))   //O(1)
+                            {
+
+                                continue;  //O(1)
+                            }
+
+                            window[window_index] = ImageMatrix[ii, jj];  //O(1)
+                            window_index++;  //O(1)
+                        }
+                    }
+
+                    //___Kth element____
+
+
+                    int Largest = Sorting.KthElement(window, window.Length, k);  //O(N + K LOG N)
+                    int kk = window.Length - (k - 1);  //O(1)
+                    int Smallest = Sorting.KthElement(window, window.Length, kk);   //O(N + K LOG N)
+
+
+                    for (int y = 0; y < window.Length; y++) //O(window.length)
+                    {
+                        if (window[y] < Largest && window[y] > Smallest)  //O(1)
+                        {
+
+                            Sum += window[y];  //O(1)
+                            counter++;   //O(1)
+
+                        }
+
+                    }
+                    if (counter == 0)  //O(1)
+                        counter = 1; //O(1)
+
+                    byte mean = (byte)(Sum / counter); //O(1)
+                   result[i, j] = mean;  //O(1)
+                }
+            }
+
+            return result;  //O(1)
+        }
+
+    }
+}
